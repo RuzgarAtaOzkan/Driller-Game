fe5578489e2090328f7ecf4dc5302243a7ce2423
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-03-22 19:06:11 +0300

    terminal edit in terminal commit

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,8 @@ public class CameraFollow : MonoBehaviour
 {
     Transform targetDriller;
     Vector3 offset = new Vector3(0f, 25f, 0f);
-
+    
+    // terminal edit wow 3
     private void Start()
     {
         targetDriller = GameObject.Find("Driller").transform;
